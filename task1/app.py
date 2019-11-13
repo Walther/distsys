7068c5198bb341b57fd42c008afee7cee2579ed7
MJL7068 <mattil@netti.fi>
2019-11-13 18:01:52 +0200

    calculates a random temperature and displays it

--- task1/app.py
+++ task1/app.py
@@ -1,13 +1,14 @@
 import os
+import random
 from flask import Flask
 app = Flask(__name__)
 
 city = (os.environ['CITY'])
-
+temp = random.normalvariate(15, 5)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, %s!' % city
+    return 'Hello, %s! The temperature is currently %.1f degrees celcius.' % (city, temp)
 
 
 if __name__ == '__main__':
